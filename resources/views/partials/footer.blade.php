8ab28b6000b5bd81c14056a8632eddfb03df8388
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-06-10 23:11:18 +0200

    add footer add bg in footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<footer id="site_footer">
+
+    <div class="footer_top">
+
+    </div>
+    <!-- /.footer_top -->
+
+    <div class="footer_bottom">
+
+    </div>
+    <!-- /.footer_bottom -->
+
+</footer>
+<!-- /#site_footer -->
