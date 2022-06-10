e7d5041ab735abd52b26309f20a661a1059b8060
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-06-11 00:26:04 +0200

    add menu in footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,232 @@
+
 <footer id="site_footer">
 
     <div class="footer_top">
 
+        <div class="container py-5">
+
+            <div class="row">
+
+                <div class="col-6">
+
+                    <div class="row">
+
+                        <div class="col-4">
+
+                            <h3 class="text-uppercase text-white">
+                                DC COMICS
+                            </h3>
+                            <!-- /.footer_menu_title -->
+
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Charachters
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Comics
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Movies
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        TV
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Games
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Videos
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        News
+                                    </a>
+                                </li>
+
+                            </ul>
+
+                            <h3 class="text-uppercase text-white">
+                               Shop
+                            </h3>
+                            <!-- /.footer_menu_title -->
+
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Shop DC
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shop DC Collectibles 
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <!-- /.col -->
+
+                        <div class="col-4">
+
+                            <h3 class="text-uppercase text-white">
+                                DC 
+                            </h3>
+                            <!-- /.footer_menu_title -->
+
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Terms Of Use
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Privacy policy(New)
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Ad Choices
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Advertising
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Jobs
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Subscriptions
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Talent Workshops
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        CPSC Certificates
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Ratings
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shop Help
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Contact Us
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <!-- /.col -->
+
+                        <div class="col-4">
+
+                            <h3 class="text-uppercase text-white">
+                               Sites
+                            </h3>
+                            <!-- /.footer_menu_title -->
+
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        DC
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        MAD Magazine
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Kids
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Universe
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        DC Power Visa
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <!-- /.col -->
+
+                        <!-- <div class="col-4"> -->
+
+                            <!-- <h3 class="text-uppercase text-white">
+                               Shop
+                            </h3> -->
+                            <!-- /.footer_menu_title -->
+
+                        <!--     <ul>
+                                <li>
+                                    <a href="#">
+                                        Shop DC
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Shop DC Collectibles 
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div> -->
+                        <!-- /.col -->
+
+                    </div>
+                    <!-- /.row -->
+
+                    <div class="footer_copyright">
+
+                        <p> 
+                            All site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a>
+                        </p>
+                        
+
+                    </div>
+
+                    
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+        
     </div>
     <!-- /.footer_top -->
 
